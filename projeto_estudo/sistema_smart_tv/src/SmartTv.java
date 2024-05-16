374907083ff8de0a299d1e7371150b50277fc585
Clayton Sales <claytonsalesdev@gmail.com>
2024-05-16 19:23:44 -0300

    .
    .

---
+++ projeto_estudo/sistema_smart_tv/src/SmartTv.java
@@ -0,0 +1,14 @@
+package projeto_estudo.sistema_smart_tv.src;
+
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+    
+    public void ligar(){
+        ligada=true;
+    }
+    public void desligar(){
+        ligada=false
+    }
+}
