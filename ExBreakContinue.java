2c059f1dbe4cbfaf22afe05ad59d66f0e3191f84
Clayton Sales <claytonsalesdev@gmail.com>
2024-06-01 14:31:58 -0300

    .
    .

---
+++ ExBreakContinue.java
@@ -0,0 +1,12 @@
+public class ExBreakContinue {
+    
+    public static void main(String[] args) {
+        for(int numero = 1; numero <=10; numero ++){
+            if(numero == 7)
+                continue;
+
+            System.out.println(numero);
+        }
+    }
+    
+}
