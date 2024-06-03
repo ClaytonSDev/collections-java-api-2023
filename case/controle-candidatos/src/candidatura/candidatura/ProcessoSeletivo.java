d18683170d47a1ca885674857d5932c3b1b7db8c
Clayton Sales <claytonsalesdev@gmail.com>
2024-06-03 16:21:58 -0300

    .
    .

--- case/controle-candidatos/src/candidatura/candidatura/ProcessoSeletivo.java
+++ case/controle-candidatos/src/candidatura/candidatura/ProcessoSeletivo.java
@@ -2,12 +2,18 @@ package candidatura;
 import  java.util.concurrent.ThreadLocalRandom;
 public class ProcessoSeletivo {
     public static void main(String[] args) throws Exception {
-        selecaoCandidatos();
+        imprimirSelecionados();
 
     }
 
     static void imprimirSelecionados() {
         String[] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        
+        for(int indice=0; indice < candidatos.length;indice++) {
+            System.out.println("O candidato de n " + (indice+1) + " é o " + candidatos[indice]);
+        }
 
     }
 
@@ -33,7 +39,7 @@ public class ProcessoSeletivo {
     static double valorPretendido(){
         return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
-    static void analisarCandidato(double salarioPretendido) {
+    static void analisarCandidatos(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido) {
             System.out.println("LIGAR PARA O CANDIDATO");
